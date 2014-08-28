b76bbccdd81f37dfac9693a3427f1a002a0f83fe
Kim Wu <kimwu@live.hk>
2014-08-28 17:35:58 +0800

    Inport to Github

---
+++ DataImport.jsx
@@ -0,0 +1,142 @@
+﻿#target illustrator; 
+
+/*
+ *Copyright Kim Wu  kimwu@live.hk
+ */
+
+var isDebug = false;
+var charFrom, charTo;
+var charStyle, charAttr;
+var docRef = app.activeDocument;
+var tfCoo = docRef.pageItems.getByName("CountryOfOrigin");
+var cooContents = "";
+
+function filterFiles(file) {
+    while (file.alias) {
+        file = file.resolve();
+        if (file == null) { return false }
+    }
+    if (file.constructor.name == "Folder") return true
+    var extn = file.name.toLowerCase().slice(file.name.lastIndexOf("."));
+        switch (extn) {
+            case ".csv" : return true;
+    default : return false
+    }
+}
+// get Fonts by fonts name
+function getFont(fontName){
+        try {
+            var tFont = app.textFonts.getByName(fontName);
+        }
+        catch(e){
+            alert("Fonts \"" + fontName + "\"  not found.", "Worning")
+            try{
+                var tFont = app.textFonts.getByName("DINCond-Medium");
+            }
+            catch(e){
+                var tFont = app.textFonts[0]
+             };
+         }
+        return tFont;
+};
+
+function getCharStyle(charStyleName){
+            try {
+                    var charStyle = app.textFonts.getByName(charStyleName);
+               }
+             catch ( e){
+                    var charStyle = docRef.characterStyles.add(charStyleName);
+                }
+            return charStyle;
+};
+
+
+var dfFolder=Folder.desktop;
+
+if (File.fs == "Windows"){
+    var   fileFillter =  "CSV File:*.csv";
+    }
+else if (file.fs == "Macintosh"){
+    var fileFillter = filterFiles ;
+    }
+
+if (isDebug) {
+    var dataFile = File("/P/jsx4ai/SourceData.csv");
+    }
+else{
+    var dataFile = dfFolder.openDlg("Please select a CSV file", fileFillter, false);
+}    
+
+dataFile.open("r");
+
+var ln = "", lnData;
+var idx = 0;
+var contents = [];
+
+//Read data from csv file, split into object list. 
+do {
+    ln = dataFile.readln();
+    //dataFile.current
+    if (ln.replace(/(^\s*)|(\s*$)/g,"").length > 0 &&  ln[0] !=";" && ln[0] !="#"){
+        lnData = ln.split(" || ");
+        contents[contents.length] ={};
+        for (index =0; index < lnData.length; index++) {
+            tmpData = lnData[index].split(":");
+            contents[contents.length - 1][tmpData[0].replace(/(^\s*)|(\s*$)/g,"")] = tmpData[1].replace(/(^\s*)|(\s*$)/g,"");
+        }
+    }
+}
+while (dataFile.eof == false);
+
+var cooData = [];
+var defaultStyle = [];
+
+for (index = 0; index < contents.length; index++) {
+    if (contents[index].dataType == "COOData"){
+        cooData[cooData.length] = contents[index];
+     }
+    else if (contents[index].dataType == "DEFAULT"){
+        defaultStyle[defaultStyle.length] = contents[index];
+     }
+}
+
+if  (defaultStyle[0] == null){
+    defaultStyle[0] = {lang:"ALL", charSize:5.5, fontName:"DINCond-Medium", text:"0123456789%\/"};
+}
+
+
+// Join all COO  contents and set the position. 
+for (index = 0; index < cooData.length; index++) {
+    cooData[index].position = cooContents.length;
+    if ( index == (cooData.length - 1)) {
+        cooContents += cooData[index].text;
+        }
+    else {
+        cooContents += cooData[index].text + " \/ ";
+        }
+} 
+// add coo contents to text frame. 
+tfCoo.textRange.contents = cooContents;
+
+// set  COO Char Sytle
+for (index = 0; index < cooData.length; index++) {
+        charStyle = getCharStyle(cooData[index].lang + " Style");
+        charAttr = charStyle.characterAttributes;
+        charAttr.size = cooData[index].charSize;
+        charAttr.textFont = getFont(cooData[index].fontName);
+        if (cooData[index].position + cooData[index].text.length + 3 < tfCoo.textRange.contents.length){
+            charTo = cooData[index].position + cooData[index].text.length + 3;
+            }
+        else{
+            charTo = tfCoo.textRange.contents.length;
+            }
+        for( i = cooData[index].position; i < charTo ; i++){
+                if (tfCoo.textRange.characters[i] == "\/"){
+                    // test
+                }
+                else {
+                    charStyle.applyTo(tfCoo.textRange.characters[i]);
+                    // tfCoo.textRange.characters[i].textFont = CharAttr.textFont
+                  }
+         }
+} ;
---
+++ TemplateInitial.jsx
@@ -0,0 +1,102 @@
+﻿#target illustrator; 
+
+/*
+ *Copyright Kim Wu  kimwu@live.hk
+ * Create five textFrame
+* Size  , Fabric Content ,   Country of Origin ,   Care Instruction,  Washing Symbols
+*/
+
+var docRef, layerRef;
+var tfSize, tfFabricContent, tfCountryOfOrigin, tfCareInstruction, tfWashingSymbols;
+var piSize, piFabricContent, piCountryOfOrigin, piCareInstruction, piWashingSymbols;
+ 
+ if (app.documents.length > 0 ) {
+     docRef = app.activeDocument;
+     }
+else {
+    docRef = app.documents.add();
+    }
+ 
+ try {  
+     layerRef = docRef.layers.getByName("ImportData");
+     }
+catch(e) { 
+    layerRef = docRef.layers.add();
+    layerRef.name = "ImportData";
+    }  
+
+try {
+    tfSize = docRef.pageItems.getByName("Size");
+    }
+catch(e) {
+    //alert(e.message,"Warning", true);
+    piSize = docRef.pathItems.rectangle( -110, docRef.height - 20, docRef.width * 0.40, docRef.height * 0.25 )
+    tfSize = docRef.textFrames.areaText(piSize);    
+    tfSize.name = "Size";
+    tfSize.contents = "Size";
+    tfSize.position = [-110, docRef.height - 20];
+    }
+
+try {
+    tfFabricContent = docRef.pageItems.getByName("FabricContent");
+    }
+catch(e) {
+    piFabricContent = docRef.pathItems.rectangle( 10, docRef.height - 240, docRef.width * 0.40, docRef.height * 0.25 )
+    tfFabricContent = docRef.textFrames.areaText(piFabricContent);    
+    tfFabricContent.name = "FabricContent";
+    tfFabricContent.contents = "Fabric Content";
+    tfFabricContent.position = [10, docRef.height - 240];
+    }
+
+try {
+    tfCountryOfOrigin = docRef.pageItems.getByName("CountryOfOrigin");
+    }
+catch(e) {
+    //alert(e.message,"Warning", true);
+    //tfCountryOfOrigin = layerRef.textFrames.add();
+    piCountryOfOrigin = docRef.pathItems.rectangle( docRef.width * 0.52, docRef.height - 20, docRef.width * 0.40, docRef.height * 0.25 )
+    tfCountryOfOrigin = docRef.textFrames.areaText(piCountryOfOrigin);    
+    tfCountryOfOrigin.name = "CountryOfOrigin";
+    tfCountryOfOrigin.contents = "Country Of Origin";
+    tfCountryOfOrigin.position = [docRef.width * 0.52, docRef.height - 20];
+    }
+
+// Create a new character style
+//var charStyle = docRef.characterStyles.add("BigRed");
+// set character attributes
+//var charAttr = charStyle.characterAttributes;
+//charAttr.size = 40;
+//charAttr.tracking = -50;
+//charAttr.capitalization = FontCapsOption.ALLCAPS;
+//var redColor = new RGBColor();
+//~ redColor.red = 255;
+//~ redColor.green = 0;
+//~ redColor.blue = 0;
+//~ charAttr.fillColor = redColor;
+//~ var char2 = tfCountryOfOrigin.textRange.characters[2];
+//~ char2.le
+//~ tfCountryOfOrigin.textRange.characters[2].characterAttributes.horizontalScale = 300; 
+//~ charStyle.applyTo(tfCountryOfOrigin.textRange.words[4]);
+
+try {
+    tfCareInstruction = docRef.pageItems.getByName("CareInstruction");
+    }
+catch(e) {
+    piCareInstruction = docRef.pathItems.rectangle(docRef.width * 0.52, docRef.height - 240, docRef.width * 0.40, docRef.height * 0.25 )
+    tfCareInstruction = docRef.textFrames.areaText(piCareInstruction);    
+    tfCareInstruction.name = "CareInstruction";
+    tfCareInstruction.contents = "Care Instruction";
+    tfCareInstruction.position = [docRef.width * 0.52, docRef.height - 240];
+    }
+
+try {
+    tfWashingSymbols = docRef.pageItems.getByName("WashingSymbols");
+    }
+catch(e) {
+    //alert(e.message,"Warning", true);
+    piWashingSymbols = docRef.pathItems.rectangle( 10, docRef.height - 480, docRef.width * 0.40, docRef.height * 0.25 )
+    tfWashingSymbols = docRef.textFrames.areaText(piWashingSymbols);    
+    tfWashingSymbols.name = "WashingSymbols";
+    tfWashingSymbols.contents = "Washing Symbols";
+    tfWashingSymbols.position = [10 , docRef.height - 480];
+    }
